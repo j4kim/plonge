b9987ef34b461c92c8587ab0ac65a952291a4e44
Joaquim Perez <jivkim@gmail.com>
2026-01-21 16:57:13 +0100

    fist poc using useUserMedia from vueUse

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,53 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDevicesList, useUserMedia } from "@vueuse/core";
+import { reactive, shallowRef, useTemplateRef, watchEffect } from "vue";
+
+const currentCamera = shallowRef<string>();
+
+const { videoInputs: cameras } = useDevicesList({
+  requestPermissions: true,
+  onUpdated() {
+    if (!cameras.value.find((i) => i.deviceId === currentCamera.value))
+      currentCamera.value = cameras.value[0]?.deviceId;
+  },
+});
+
+const video = useTemplateRef("video");
+
+const { stream, enabled, start } = useUserMedia({
+  constraints: reactive({ video: { deviceId: { exact: currentCamera } } }),
+});
+
+watchEffect(() => {
+  if (video.value) video.value.srcObject = stream.value!;
+});
+
+start();
+</script>
 
 <template>
-  <h1 class="text-xl">You did it!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation
-  </p>
+  <div class="flex flex-col gap-2 items-center my-8 px-8">
+    <button @click="enabled = !enabled">
+      {{ enabled ? "Arrêter" : "Commencer" }}
+    </button>
+
+    <div
+      v-for="camera of cameras"
+      :key="camera.deviceId"
+      class="cursor-pointer"
+      :class="{ 'font-bold': currentCamera === camera.deviceId }"
+      @click="currentCamera = camera.deviceId"
+    >
+      {{ camera.label }}
+    </div>
+
+    <video
+      ref="video"
+      muted
+      autoplay
+      class="aspect-9/16 rounded-xl border max-h-[80vh]"
+    />
+  </div>
 </template>
 
 <style scoped></style>
